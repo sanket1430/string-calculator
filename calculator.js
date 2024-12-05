6b7da49d1af179f38986f84f74884145ae1d37c5
sgupta1 <sanketgupta@lucidmotors.com>
2024-12-05 16:02:42 +0530

    Add functionality to handle single numbers

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 function add(numbers) {
-    if (numbers === "") {
-      return 0;
-    }
+  if (numbers === "") {
+    return 0;
   }
-  
+  return parseInt(numbers);
+}
+
 module.exports = { add };
   
--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('./calculator');
 test('Empty string returns 0', () => {
   expect(add('')).toBe(0);
 });
+
+test('Single number returns the number', () => {
+  expect(add('1')).toBe(1);
+});
