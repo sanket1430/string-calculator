cee6f8b1c854a9e7b30461416a34ed4a5bce0174
sgupta1 <sanketgupta@lucidmotors.com>
2024-12-05 16:08:15 +0530

    Handle multiple numbers with commas

--- calculator.js
+++ calculator.js
@@ -2,7 +2,8 @@ function add(numbers) {
   if (numbers === "") {
     return 0;
   }
-  return parseInt(numbers);
+  const numList = numbers.split(',').map(num => parseInt(num));
+  return numList.reduce((acc, curr) => acc + curr, 0);
 }
 
 module.exports = { add };
--- calculator.test.js
+++ calculator.test.js
@@ -7,3 +7,7 @@ test('Empty string returns 0', () => {
 test('Single number returns the number', () => {
   expect(add('1')).toBe(1);
 });
+
+test('Two numbers are summed correctly', () => {
+  expect(add('1,5')).toBe(6);
+});
